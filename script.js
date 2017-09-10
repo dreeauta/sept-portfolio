7e3d51d985ccbd0f5b76886814665663ddb4f1bc
Andreea Uta <dreea.9965@gmail.com>
2017-09-10 18:04:38 -0400

    added contact section

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ $(document).ready(function () {
 
 // fade in projects
 
+var project = $(".skills").offset().top;
+$( ".project-container" ).hide();
+
+$(document).scroll(function() {
+    if($(this).scrollTop() > project ) {
+        $(".project-container").show();
+    }
+});
 
 
-)};
+});
