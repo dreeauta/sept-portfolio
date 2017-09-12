aecf2c061d0c36952b740c8a822cb2fc982ea2d2
Andreea Uta <dreea.9965@gmail.com>
2017-09-12 01:56:02 -0400

    updated file

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 $(document).ready(function () {
 
+  // fade in skills
+
+var skills = $(".title").offset().top;
+
+$(document).scroll(function() {
+  if($(this).scrollTop() > skills) {
+    $(".skills").show();
+  }
+})
+
+
 // fade in projects
 
 var project = $(".skills").offset().top;
 $( ".project-container" ).hide();
 
 $(document).scroll(function() {
-    if($(this).scrollTop() > project ) {
+    if($(this).scrollTop() > project + 300 ) {
         $(".project-container").show();
+    }
+});
+
+
+var about = $(".about-wrapper").offset().top;
+$( ".about-text" ).hide();
+
+$(document).scroll(function() {
+    if($(this).scrollTop() > about + 100 ) {
+        $(".about-text").show();
     }
 });
 
